50f41dce3708545b7077a668045653bfba028146
Juan Espinoza <espinozaajuan04@gmail.com>
2021-10-22 13:22:31 -0300

    Initial commit + fetch unsplash API

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = 'Mz_y5oG0R2nhq_n8pCmNo5HC3jPxZI_mIFYIiUY8NhQ';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        //catch error here
+    }
+}
+
+//On Load
+getPhotos();
